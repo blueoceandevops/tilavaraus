8591b49f8f1745d914a3d89d08c4527d6ba94b60
Mico Piira <mico.piira@gmail.com>
2017-11-04 22:38:21 +0200

    Disable service working caching non GET requests

--- src/main/webapp/service-worker.js
+++ src/main/webapp/service-worker.js
@@ -51,7 +51,7 @@ self.addEventListener('activate', event => {
 // from the network before returning it to the page.
 self.addEventListener('fetch', event => {
 	// Skip cross-origin requests, like those for Google Analytics.
-	if (event.request.url.startsWith(self.location.origin)) {
+	if (event.request.method === 'GET' && event.request.url.startsWith(self.location.origin)) {
 		event.respondWith(
 			caches.match(event.request).then(cachedResponse => {
 				if (cachedResponse) {

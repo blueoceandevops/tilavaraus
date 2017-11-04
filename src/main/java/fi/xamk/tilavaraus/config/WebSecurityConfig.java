0b2d84fbf6ec99b7f1eb44fcd8242c351fd02eca
Mico Piira <mico.piira@gmail.com>
2017-11-04 21:37:37 +0200

    Revert "Require HTTPS on production"
    This reverts commit ba80fae
    
    Revert "Require HTTPS on production"
    
    This reverts commit ba80fae

--- src/main/java/fi/xamk/tilavaraus/config/WebSecurityConfig.java
+++ src/main/java/fi/xamk/tilavaraus/config/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package fi.xamk.tilavaraus.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,7 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
-@Profile("dev")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override

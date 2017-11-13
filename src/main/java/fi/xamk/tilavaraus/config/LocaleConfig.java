485b742e547b2af1c3ae964fc7d7631745682206
Mico Piira <mico.piira@gmail.com>
2017-11-13 12:45:04 +0200

    Fix message bundle wrong locale on production

--- src/main/java/fi/xamk/tilavaraus/config/LocaleConfig.java
+++ src/main/java/fi/xamk/tilavaraus/config/LocaleConfig.java
@@ -1,7 +1,9 @@
 package fi.xamk.tilavaraus.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,6 +20,15 @@ public class LocaleConfig implements WebMvcConfigurer {
 		SessionLocaleResolver slr = new SessionLocaleResolver();
         slr.setDefaultLocale(Locale.forLanguageTag("fi-FI"));
         return slr;
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setDefaultEncoding("UTF-8");
+		messageSource.setBasename("classpath:messages");
+		messageSource.setFallbackToSystemLocale(false);
+		return messageSource;
 	}
 
 	@Bean

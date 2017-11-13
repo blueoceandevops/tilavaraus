e36963e7e3374b83cfb2fb98614caede22175a52
Mico Piira <mico.piira@gmail.com>
2017-11-13 19:32:51 +0200

    Change bootstrap border-radius to 0

--- src/main/webapp/dist/main.js
+++ src/main/webapp/dist/main.js
@@ -3511,6 +3511,7 @@
 			var rnothtmlwhite = (/[^\x20\t\r\n\f]+/g);
 
 
+
 // Convert String-formatted options into Object-formatted ones
 			function createOptions(options) {
 				var object = {};
@@ -4142,6 +4143,8 @@
 			};
 
 
+
+
 // The deferred used on DOM ready
 			var readyList = jQuery.Deferred();
 
@@ -4217,6 +4220,8 @@
 				// A fallback to window.onload, that will always work
 				window.addEventListener("load", completed);
 			}
+
+
 
 
 // Multifunctional method to get and set values of a collection
@@ -4444,6 +4449,7 @@
 			var dataPriv = new Data();
 
 			var dataUser = new Data();
+
 
 
 //	Implementation Summary
@@ -4958,6 +4964,7 @@
 			var rtagName = (/<([a-z][^\/\0>\x20\t\r\n\f]+)/i);
 
 			var rscriptType = (/^$|\/(?:java|ecma)script/i);
+
 
 
 // We have to close these tags to support XHTML (#13200)
@@ -8357,6 +8364,8 @@
 			});
 
 
+
+
 // Return jQuery for attributes-only inclusion
 
 
@@ -8601,6 +8610,7 @@
 			var nonce = jQuery.now();
 
 			var rquery = (/\?/);
+
 
 
 // Cross-browser xml parsing
@@ -9832,6 +9842,8 @@
 			});
 
 
+
+
 // Prevent auto-execution of scripts when no explicit dataType was provided (See gh-2432)
 			jQuery.ajaxPrefilter(function (s) {
 				if (s.crossDomain) {
@@ -9994,6 +10006,8 @@
 			});
 
 
+
+
 // Support: Safari 8 only
 // In Safari 8 documents created via document.implementation.createHTMLDocument
 // collapse sibling forms: the second one becomes a child of the first one.
@@ -10119,6 +10133,8 @@
 
 				return this;
 			};
+
+
 
 
 // Attach a bunch of functions for handling common AJAX events
@@ -10430,6 +10446,8 @@
 			jQuery.isArray = Array.isArray;
 			jQuery.parseJSON = JSON.parse;
 			jQuery.nodeName = nodeName;
+
+
 
 
 // Register as a named AMD module, since jQuery can be concatenated with other

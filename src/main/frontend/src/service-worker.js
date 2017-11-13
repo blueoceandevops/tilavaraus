631b093e5e9e28eb3b57269471c1e1d0337528cc
Mico Piira <mico.piira@gmail.com>
2017-11-13 21:38:47 +0200

    Handle service-worker.js with webpack

---
+++ src/main/frontend/src/service-worker.js
@@ -0,0 +1,35 @@
+const PRECACHE_URLS = [
+	'/',
+	'/?utm_source=homescreen',
+	'/dist/main.js',
+	'/dist/main.css'
+];
+
+self.addEventListener('install', event => {
+	event.waitUntil(
+		caches.open(PRECACHE)
+			.then(cache => cache.addAll(PRECACHE_URLS))
+			.then(self.skipWaiting())
+	);
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+	const currentCaches = [PRECACHE];
+	event.waitUntil(
+		caches.keys().then(cacheNames =>
+			cacheNames.filter(cacheName => !currentCaches.includes(cacheName))
+		).then(cachesToDelete =>
+			Promise.all(cachesToDelete.map(cacheToDelete =>
+				caches.delete(cacheToDelete)
+			))
+		).then(() => self.clients.claim())
+	);
+});
+
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		fetch(event.request)
+			.catch(() => caches.match(event.request))
+	);
+});

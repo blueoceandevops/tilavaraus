2bba6ecb79a18e6e6768ddcf83162d7a527e4e0f
Mico Piira <mico.piira@gmail.com>
2017-11-18 01:18:14 +0200

    Fix overlapping reservations check

--- src/main/java/fi/xamk/tilavaraus/domain/ReservationRepository.java
+++ src/main/java/fi/xamk/tilavaraus/domain/ReservationRepository.java
@@ -12,9 +12,9 @@ public interface ReservationRepository extends CrudRepository<Reservation, Long>
 	List<Reservation> findByUser(User user);
 
 	@Query("select r from Reservation r where " +
-			"((r.startTime > ?1 and r.startTime < ?2) or (r.endTime > ?1 and r.endTime < ?2)) or " +
+			"(((r.startTime > ?1 and r.startTime < ?2) or (r.endTime > ?1 and r.endTime < ?2)) or " +
 			"(r.startTime < ?1 and r.endTime > ?2) or " +
-			"(r.startTime > ?1 and r.endTime < ?2) and " +
+			"(r.startTime > ?1 and r.endTime < ?2)) and " +
 			"(r.room = ?3)")
 	List<Reservation> findOverlapping(LocalDateTime start, LocalDateTime end, Room room);
 }

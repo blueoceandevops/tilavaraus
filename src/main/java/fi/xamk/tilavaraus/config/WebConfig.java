60704b55897623f130aa7979c9d190defdc74502
Mico Piira <mico.piira@gmail.com>
2017-11-23 15:05:45 +0200

    Disable DefaultServletHandlerConfigurer because its not needed (?)

--- src/main/java/fi/xamk/tilavaraus/config/WebConfig.java
+++ src/main/java/fi/xamk/tilavaraus/config/WebConfig.java
@@ -1,13 +1,9 @@
 package fi.xamk.tilavaraus.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-		configurer.enable();
-	}
+
 }

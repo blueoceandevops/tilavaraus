c0190c6330184243b54240d36487d0dae126ed3e
Mico Piira <mico.piira@gmail.com>
2017-11-23 16:56:02 +0200

    Only populate rooms and additional services when necessary

--- src/main/java/fi/xamk/tilavaraus/DemoApplication.java
+++ src/main/java/fi/xamk/tilavaraus/DemoApplication.java
@@ -46,15 +46,16 @@ public class DemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner populateRooms(RoomRepository roomRepository) {
-		return (args) ->
-				roomRepository.saveAll(Arrays.asList(
-						createRoom(1L, "Mikpolisali", 117, 160.0, "/img/mikpolisali.jpg"),
-						createRoom(2L, "Kuitula", 22, 200.0, "https://www.xamkravintolat.fi/wp-content/uploads/sites/2/2017/02/Kuitula_ylakuva-2-1920x725.jpg"),
-						createRoom(3L, "Tallin vintti", 40, 90.0, "https://www.xamkravintolat.fi/wp-content/uploads/sites/2/2017/02/talli_ylakerta-1920x725.jpg"),
-						createRoom(4L, "MA325", 9999, 80.0, "https://via.placeholder.com/350x150"),
-						createRoom(5L, "MB124", 9999, 60.0, "https://via.placeholder.com/350x150")
+	public CommandLineRunner populateAdditionalServices(AdditionalServiceRepository additionalServiceRepository) {
+		return (args) -> {
+			if (additionalServiceRepository.count() == 0) {
+				additionalServiceRepository.saveAll(Arrays.asList(
+						createAdditionalService(1L, "foodAndDrink", BigDecimal.valueOf(10.00)),
+						createAdditionalService(2L, "itSupport", BigDecimal.valueOf(10.00)),
+						createAdditionalService(3L, "janitor", BigDecimal.valueOf(10.00))
 				));
+			}
+		};
 	}
 
 	private AdditionalService createAdditionalService(Long id, String name, BigDecimal price) {
@@ -66,12 +67,19 @@ public class DemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner populateAdditionalServices(AdditionalServiceRepository additionalServiceRepository) {
-		return (args) -> additionalServiceRepository.saveAll(Arrays.asList(
-				createAdditionalService(1L, "foodAndDrink", BigDecimal.valueOf(10.00)),
-				createAdditionalService(2L, "itSupport", BigDecimal.valueOf(10.00)),
-				createAdditionalService(3L, "janitor", BigDecimal.valueOf(10.00))
-		));
+	public CommandLineRunner populateRooms(RoomRepository roomRepository) {
+		return (args) ->
+		{
+			if (roomRepository.count() == 0) {
+				roomRepository.saveAll(Arrays.asList(
+						createRoom(1L, "Mikpolisali", 117, 160.0, "/img/mikpolisali.jpg"),
+						createRoom(2L, "Kuitula", 22, 200.0, "https://www.xamkravintolat.fi/wp-content/uploads/sites/2/2017/02/Kuitula_ylakuva-2-1920x725.jpg"),
+						createRoom(3L, "Tallin vintti", 40, 90.0, "https://www.xamkravintolat.fi/wp-content/uploads/sites/2/2017/02/talli_ylakerta-1920x725.jpg"),
+						createRoom(4L, "MA325", 9999, 80.0, "https://via.placeholder.com/350x150"),
+						createRoom(5L, "MB124", 9999, 60.0, "https://via.placeholder.com/350x150")
+				));
+			}
+		};
 	}
 
 }

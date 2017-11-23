bffa22f040ee23d9cd8bd0ee786d22b52c3fa6c4
Mico Piira <mico.piira@gmail.com>
2017-11-23 23:55:25 +0200

    Some validation cleaning

--- src/main/java/fi/xamk/tilavaraus/web/ApiController.java
+++ src/main/java/fi/xamk/tilavaraus/web/ApiController.java
@@ -1,7 +1,9 @@
 package fi.xamk.tilavaraus.web;
 
 import fi.xamk.tilavaraus.domain.Reservation;
-import fi.xamk.tilavaraus.domain.Room;
+import fi.xamk.tilavaraus.domain.validation.ReservationValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -10,10 +12,20 @@ import java.math.BigDecimal;
 @RequestMapping("/api")
 @RestController
 public class ApiController {
+	private final ReservationValidator reservationValidator;
+
+	@Autowired
+	public ApiController(ReservationValidator reservationValidator) {
+		this.reservationValidator = reservationValidator;
+	}
+
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		binder.addValidators(reservationValidator);
+	}
 
 	@PostMapping("/calculatePrice")
-	public BigDecimal calculatePrice(@Valid @ModelAttribute("reservation") Reservation reservation, @RequestParam("roomId") Room room) {
-		reservation.setRoom(room);
+	public BigDecimal calculatePrice(@Valid @ModelAttribute("reservation") Reservation reservation) {
 		return reservation.getTotalPrice();
 	}
 
